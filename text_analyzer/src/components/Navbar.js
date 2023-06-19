92c7d9acb189c6cce7d3d206215e9d9fe2dbaf0e
Abhishek280999 <darwekar155@gmail.com>
2023-06-19 14:56:50 +0530

    done 3

--- text_analyzer/src/components/Navbar.js
+++ text_analyzer/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
